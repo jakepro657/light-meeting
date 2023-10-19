c7119cb5d330589315ad28c5cec4959a303fdf74
jakepro657 <jakepro657@gmail.com>
2023-10-19 15:59:21 +0900

    configure: design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
@@ -14,7 +14,11 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    fontFamily: {
+      "GmarketSansMedium": ["GmarketSansMedium"],
+      "PretendardRegular": ["PretendardRegular"],
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animated')],
 }
 export default config
